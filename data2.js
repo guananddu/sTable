9934774c7121f2f6f17988119ee8af3055909fdf
Pooky <guananddu@qq.com>
2015-03-13 18:15:10 +0800

    组件优化
    1，决定后期使用采用less
    2，点击子表展开的bug修改
    3，添加自定义排序功能

---
+++ data2.js
@@ -0,0 +1,156 @@
+/**
+ * 模拟测试数据，其中的每一个item中的lll字段属于子表字段
+ */
+var data2 = [{
+		abc : 222123,
+		username : "bbbxxx嘎嘎嘎！！！！",
+		lammx : "bbbxxx",
+		asdcxz : true,
+		mnb : 22.22,
+		lll : [{
+				vvvz : "33",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "5",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}
+		]
+	}, {
+		abc : 2243,
+		username : "bbb",
+		lammx : "bbb",
+		asdcxz : true,
+		mnb : 22.22,
+		lll : [{
+				vvvz : "33",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "5",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "33",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "5",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "5",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}
+		]
+	}, {
+		abc : 22445,
+		username : "bbb",
+		lammx : "bbb",
+		asdcxz : true,
+		mnb : 22.22,
+		lll : []
+	}, {
+		abc : 226,
+		username : "bbb",
+		lammx : "bbb",
+		asdcxz : true,
+		mnb : 22.22,
+		lll : [{
+				vvvz : "33",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "5",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "5",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "77",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}
+		]
+	}, {
+		abc : 28,
+		username : "bbb",
+		lammx : "bbb",
+		asdcxz : true,
+		mnb : 22.22,
+		lll : []
+	}, {
+		abc : 2,
+		username : "bbb",
+		lammx : "bbb",
+		asdcxz : true,
+		mnb : 22.22,
+		lll : [{
+				vvvz : "33",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "5",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "33",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "5",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "33",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "5",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "33",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "5",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "33",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}, {
+				vvvz : "5",
+				poi : 22,
+				qqq : 22,
+				bbbbbbbb : 22
+			}
+		]
+	}
+];
---
+++ idt-config.js
@@ -0,0 +1,286 @@
+/**
+ * idt全局配置文件
+ */
+
+/**
+ * 在项目根目录执行build，需要在根目录建立一个`module.conf`文件
+ * 它是用来配置module模块，如下内容的文件：
+ *   {
+ *       "baseUrl": "src",
+ *       "packages": [ ],
+ *       "combine": {
+ *           "custom/search/main": 1,
+ *           "center/mine/main": 1,
+ *           "custom/position/main": 1,
+ *           "custom/city/main": 1,
+ *           "custom/list/main": 1,
+ *           "custom/mine/main": 1,
+ *           "custom/salary/main": 1,
+ *           "custom/stages/main": 1
+ *       }
+ *   }
+ */
+
+/**
+ * 在根目录构建，可以新建一个`copyright.txt`文本文件来给所有build出来的文件，添加统一的版权
+ * 声明，例如：
+ * 
+ * "! 2014 Lagou Inc. All Rights Reserved"
+ *
+ * 【此文件可以自动生成了，可以在本配置文件中写好配置即可】
+ */
+
+var path = require( 'path' );
+
+// 当前文件夹路径【无需修改】
+var webContent = __dirname;
+
+// web inspector remote 的配置及其使用参看：
+// http://people.apache.org/~pmuellr/weinre-docs/latest/Running.html
+// debug username, 关闭调试: `off`，如果设置此项，则开启调试，不设置即为`off`关闭
+var wsWeinreDebug = '' || 'off';
+// default 8080
+var weinreDebugPort = '' || '8080'; 
+// default localhost 
+var weinreDebugHost = '' || 'localhost'; 
+
+// 执行`build`的时候字符串替换的配置，下面此句的意思是，所有的`html`文件中的`#parse( "`
+// 均替换成`#parse( "mobile/tpl/`
+// 【此项视情况而定】
+var replaces = {
+    exclude: [ '*' ],
+    include: [ '*.html' ],
+    replacements: [ { from: /\#parse\( \"/g, to: '#parse( "mobile/tpl/' } ]
+};
+
+// 二级目录设置【一般不需修改】
+var secondary = '';
+// build子目录时需要用到【无需修改】
+var buildLevel = '';
+
+module.exports = {
+
+    // webserver的端口号
+    webPort: '8003',
+
+    // webserver的项目根目录【一般不需修改】，即为当前`pwd`的目录
+    webContent: webContent,
+
+    // velocity的template模板根目录【只需要修改最后一个参数即可】
+    templates: path.join( webContent, secondary, '.' ),
+
+    // 单路径整体build【String】
+    // buildPath: '../outs/outall',
+
+    // 多路径整体build【Array】
+    // buildPath: [ '../outs/o1', '../outs/o2' ],
+
+    // 多路径整体build，但是需要资源分离【Object】
+    buildPath: {
+
+        // 键名是需要存留的文件，键值是对应的build路径
+
+        '.js': '../outs/outjs/mobile',
+        '.css': '../outs/outcss/mobile',
+        '.jpg|.jpeg|.gif|.png': '../outs/outimg/mobile',
+        '.html|.htm': '../outs/outtemplate/mobile',
+
+        // template的buildpath直接诶通过templates路径指定
+
+    },
+
+    // 反向代理配置【按需配置】，键名可以随意，只要是每一个的匹配规则
+    reverseProxyMap: {
+
+        tpl: {
+            pattern: /^\/custom\//,
+            replace: '/tpl/custom/'
+        }
+
+    },
+
+    // 批处理按照对应的模板文件，生成相同模板，不同静态页面的配置
+    batch2Html: {
+
+        // 指定数据源目录
+        path: path.join( webContent, 'mock/batch' ),
+
+        // 指定静态文件输出目录
+        out: path.join( webContent, 'mock/batch_out' ),
+
+        // 模板路径在上面配置的`template`路径下
+        tpl: '404.html',
+
+        // 数据的data路径是自动生成
+        // 在 path.join( 'mock/batch', tpl[ 上面的tpl路径去掉.html的文件夹 ] )
+
+    },
+
+    // 是否对'*.atpl.js'这种请求进行判断【一般不需修改】
+    // false: 读取'*.atpl'
+    // true: 读取'*.atpl.js'
+    wsNoNeed2TrimDotJs: false,
+
+    // mock 相关配置【一般不需修改】
+    mockCommon: 'commonmock/common.js',
+    mockVelocity: path.join( webContent, secondary, 'mock/velocity' ),
+    mockAjax: path.join( webContent, secondary, 'mock/ajax' ),
+
+    /**
+     * web server 中间层
+     * requester 是一个请求器，可以用来做反向代理等等
+     * 
+     * @param  {Object} connect
+     * @param  {Object} options
+     * @param  {Array}  middlewares 系统中间层
+     * @param  {Object} rtool       
+     * { 
+     *     requester: 请求器, 
+     *     defaulthostp: '默认的本地域名' 
+     * }
+     * @return {Array}  中间层数组
+     */
+    middlewares: function( connect, options, middlewares, rtool ) {
+
+        // how to inject a custom middleware into the array of default 
+        // middlewares
+        // 
+        // html middlewares example:
+        // 
+        // middlewares.unshift( function( req, res, next ) {
+        //     if ( utils.isHtml( req ) )
+        //         return handlerHtml.run( req, res, next, config );
+        //     return next();
+        // } );
+
+        middlewares.unshift( function( req, res, next ) {
+            // 这是一个可以自定义的中间层
+            console.log( 'user middleware, request url: ' + req.url );
+            return next();
+        } );
+
+        // 如果需要请求对应的线上数据或者其他域的数据，可以使用 requester 来请求
+        // requester的使用参见：
+        // https://www.npmjs.org/package/request
+        // console.log( rtool.requester, rtool.defaulthostp );
+
+        return middlewares;
+
+    },
+
+    wsWeinreDebug: wsWeinreDebug,
+    weinreDebugPort: weinreDebugPort,
+    weinreDebugHost: weinreDebugHost,
+
+    // 【无需修改】
+    input: path.resolve( webContent, buildLevel ),
+    // 【无需修改】
+    output: path.resolve( __dirname, '.output', buildLevel ),
+    // 【无需修改】
+    replaces: replaces,
+
+    // build 处理器配置
+    getProcessors: function() {
+        var lessProcessor = new LessCompiler( {
+            // 【此项先不做配置了】
+            // 
+            // 在build的时候，没有必要对每一个less都进行编译，因为很可能有一些被import
+            // 入的less文件会编译出错【不会影响整体样式】，只需在include中写入每一个页面的
+            // 入口main.less文件即可
+            // 
+            // exclude: [ '*.less' ],
+            // include: [ 
+            //     'src/center/mine/css/mine.less',
+            //     'src/custom/search/css/search.less'
+            //  ]
+        } );
+        var cssProcessor = new CssCompressor( {
+            compressOptions: {
+                keepBreaks: false
+            }
+        } );
+        var html2JsCompilerProcessor = new Html2JsCompiler( {
+            mode: 'compress',
+            extnames: [ 'atpl' ],
+            combine: true,
+            exclude: [ '*' ],
+            include: [ '*.atpl' ]
+        } ); 
+        var moduleProcessor = new ModuleCompiler();
+        var jsProcessor = new JsCompressor();
+        var pathMapperProcessor = new PathMapper();
+        var addCopyright = new AddCopyright();
+        var stringReplace = new StringReplace( replaces );
+
+        return {
+            // 默认的build不需要压缩，以便开发（联调）的时候，利于调试
+            'default': [ lessProcessor, html2JsCompilerProcessor,
+                        moduleProcessor, pathMapperProcessor, 
+                        stringReplace ],
+            // 在最后联调成功以后，要进行release，会进行代码压缩等处理
+            'release': [
+                lessProcessor, cssProcessor, html2JsCompilerProcessor,
+                moduleProcessor,jsProcessor, pathMapperProcessor, 
+                addCopyright, stringReplace
+            ]
+        };
+    },
+
+    // build 中需要排除的文件（夹）
+    exclude: [
+        'demo',
+        'Gruntfile.js',
+        'tests',
+        'test',
+        'examples',
+        'vendor',
+        'demo',
+        'tool',
+        'doc',
+        'test',
+        'make',
+        'module.conf',
+        'dep/packages.manifest',
+        'dep/*/*/test',
+        'dep/*/*/doc',
+        'dep/*/*/demo',
+        'dep/*/*/tool',
+        'dep/*/*/*.md',
+        'dep/*/*/package.json',
+        'edp-*',
+        '.edpproj',
+        '.svn',
+        '.git',
+        '.gitignore',
+        '.idea',
+        '.project',
+        'Desktop.ini',
+        'Thumbs.db',
+        '.DS_Store',
+        '*.tmp',
+        '*.bak',
+        '*.swp',
+        '*.psd',
+        'copyright.txt',
+        'package.json',
+        'README.md',
+        'mobile',
+        'idt-config.js',
+        'idt-config.js.bak',
+        // pagefooter.html为建立下载模板，build会出错，故排除，但是需要单独处理
+        'pagefooter.html',
+        'WEB-INF',
+        'mock'
+    ],
+
+    // 和上面的保持一致就可以，idt单独对其进行copy
+    idtCopyList: [],
+
+    // do not modify
+    injectProcessor: function( processors ) {
+        for ( var key in processors ) {
+            global[ key ] = processors[ key ];
+        }
+    }
+
+};
